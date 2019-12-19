46373c20c4076570255d9f25428a4413d83cc4ef
Vishal <46787778+mew-two-github@users.noreply.github.com>
2019-12-19 22:00:25 +0530

    Added test cases for hacker earth prelims

---
+++ test_cases/Q7/og.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define max(a,b) ((a>b)?a:b)
+#define cin f1
+#define cout f2
+using namespace std;
+using namespace std::chrono;
+ll timet=0;
+
+
+fstream f1,f2;
+void solve()
+{
+	int n;
+    cin>>n;
+    int p;
+    vector<int> edge[n+1];
+    vector<int> oe[n+1];
+    int depth[n+1]={-1};
+    queue<int> q;
+    for(int i=1;i<n;i++)
+    {
+    	int x,y;
+       	cin>>x>>y;
+       	edge[x].push_back(y);
+       	oe[y].push_back(x);
+    }
+    int root=-1;
+    for(int i=1;i<n+1;i++)
+    {
+    	if(oe[i].size()==0)
+    		root=i;
+    }
+    q.push(root);
+    depth[root]=0;
+    auto start=high_resolution_clock::now();
+    while(q.empty()==false)
+    {
+        int n1=q.front();
+        q.pop();
+        for(auto i=edge[n1].begin();i!=edge[n1].end();i++)
+        {
+            depth[*i]=depth[n1]+1;
+            q.push(*i);
+        }
+    }
+    int ans=0;
+    for(int i=0;i<n+1;i++)
+        if(depth[i]>ans)
+            ans=depth[i];
+	auto stop=high_resolution_clock::now();
+    auto duration=duration_cast<microseconds>(stop-start);
+    timet+=duration.count();
+    cout<<ans<<'\n';
+}
+
+void calc1()
+{
+	int t;
+	cin>>t;
+	timet=0;
+	while(t--)
+	{
+		solve();
+	}
+	printf("%d microseconds\n",timet);
+}
+
+int main()
+{
+    for(int t=0;t<10;t++)
+    {
+    	string s1="t",s2="o";
+        s1+=(t+48);
+    	s2+=(t+48);
+    	s1+=".txt";
+    	s2+=".txt";
+    	f1.open(s1,ios::in);
+    	f2.open(s2,ios::out);
+        calc1();
+        f1.close();
+        f2.close();
+    }
+}
---
+++ test_cases/Q7/tcg.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define RAND(x,y) x+(rand()%(y-x+1))
+
+using namespace std;
+int main()
+{
+    srand(time(0));
+    ll times=10;
+    for(ll times=0;times<10;times++)
+    {
+        string sname="t";
+        sname+=(times+48);
+        sname+=".txt";
+        fstream f(sname,ios::out);
+        ll t=RAND(7,10);
+        f<<t<<endl;
+        while(t--)
+        {
+        	ll n=RAND(8000,10000);
+        	f<<n<<endl;
+        	int a[n];
+        	for(ll i=0;i<n;i++)
+        		a[i]=i+1;
+        	vector<pair<ll,ll>> e;
+        	for(ll i=1;i<n;i++)
+        	{
+        		ll p=RAND(0,i-1);
+        		e.push_back(make_pair(i,p));
+        	}
+        	random_shuffle(a,a+n);
+        	for(auto i:e)
+        		f<<a[i.first]<<" "<<a[i.second]<<endl;
+        }
+        f.close();
+    }
+    return 0;
+}
